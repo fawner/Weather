1ac571412a0fcf90a5a0f108dd0df48da56222f6
fawn <297032113@qq.com>
2016-11-09 14:19:39 +0800

    无修改

--- app/src/main/java/com/fast/weather/QueryGET.java
+++ app/src/main/java/com/fast/weather/QueryGET.java
@@ -4,9 +4,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -80,7 +82,7 @@ public class QueryGET {
             conn.setDoInput(true);
             conn.setRequestMethod("GET");
             conn.connect();
-
+            
             InputStream is = conn.getInputStream();
             reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             String strRead = null;
