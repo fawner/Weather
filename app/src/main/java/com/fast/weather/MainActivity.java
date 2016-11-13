fc690edf7abf8d5d2aed20ca293d3542f97a2ad6
fawn <297032113@qq.com>
2016-11-13 16:22:03 +0800

    添加pinyin4j.jar包并创建中文转拼音类Pinyin

--- app/src/main/java/com/fast/weather/MainActivity.java
+++ app/src/main/java/com/fast/weather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fast.weather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
