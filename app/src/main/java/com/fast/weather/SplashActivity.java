71bac7a03e93a5c954588adfcbdc6457b248e34b
fawn <297032113@qq.com>
2016-11-19 17:05:12 +0800

    启动界面监测网络设置

--- app/src/main/java/com/fast/weather/SplashActivity.java
+++ app/src/main/java/com/fast/weather/SplashActivity.java
@@ -1,14 +1,19 @@
 package com.fast.weather;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
     private Handler handler = new Handler();
@@ -28,22 +33,68 @@ public class SplashActivity extends AppCompatActivity {
         SharedPreferences.Editor edit = shared.edit();
         isFirst = shared.getBoolean("isFirst",true);
         str_cityId = shared.getString("setCityId","");
-        //是否第一次进入
-        if (isFirst || str_cityId.equals("")) {
-            intent = new Intent(SplashActivity.this, SetActivity.class);
-            edit.putBoolean("isFirst",false);
-            edit.commit();
-        }else{
-            intent = new Intent(SplashActivity.this, MainActivity.class);
-        }
-        //延时
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(intent);
-                finish();
-            }
-        }, 2000);
 
+        if (!isOpenNetwork()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+            builder.setTitle("没有可用的网络").setMessage("是否对网络进行设置?");
+
+            builder.setPositiveButton("是", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = null;
+
+                    try {
+                        String sdkVersion = android.os.Build.VERSION.SDK;
+                        if(Integer.valueOf(sdkVersion) > 10) {
+                            intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                        }else {
+                            intent = new Intent();
+                            ComponentName comp = new ComponentName("com.android.settings", "com.android.settings.WirelessSettings");
+                            intent.setComponent(comp);
+                            intent.setAction("android.intent.action.VIEW");
+                        }
+                        SplashActivity.this.startActivity(intent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).setNegativeButton("否", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    finish();
+                }
+            }).show();
+        }else{
+            //是否第一次进入
+            if (isFirst || str_cityId.equals("")) {
+                intent = new Intent(SplashActivity.this, SetActivity.class);
+                edit.putBoolean("isFirst",false);
+                edit.commit();
+            }else{
+                intent = new Intent(SplashActivity.this, MainActivity.class);
+            }
+            //延时
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+        }
+
+
+
+    }
+    private boolean isOpenNetwork() {
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+
+        if(networkInfo!= null) {
+            return connManager.getActiveNetworkInfo().isAvailable();
+        }
+
+        return false;
     }
 }
